05446e355d6aac53ac790378cf321a8fef84bf38
Issa Hassane <issahassane1@hotmail.com>
2022-06-06 17:35:51 +0530

    final product submission

---
+++ zomato_clone_back_end/controllers/locations.js
@@ -0,0 +1,34 @@
+const locations = require("../models/locations");
+
+exports.getlocations = (req, res) => {
+  locations
+    .find()
+    .then((response) => {
+      res
+        .status(200)
+        .json({
+          message: "Locations Fetched Succesfully",
+          locations: response,
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error", error: err });
+    });
+};
+
+exports.getLocationById = (req, res) => {
+  const location_id = req.params.LocId;
+  locations
+    .find({ name: location_id })
+    .then((response) => {
+      res
+        .status(200)
+        .json({
+          message: "Locations Fetched Succesfully",
+          locations: response,
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error", error: err });
+    });
+};
